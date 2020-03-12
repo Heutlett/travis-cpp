0dfd33385819cd6e2f43721c38f151eb41a304a8
Heutlett <44324449+Heutlett@users.noreply.github.com>
2020-03-12 02:50:18 -0600

    Added .gitignore .travis.yml

---
+++ calculadora.h
@@ -0,0 +1,31 @@
+//
+// Created by carlo on 3/12/2020.
+//
+
+#ifndef TRAVIS_CPP_CALCULADORA_H
+#define TRAVIS_CPP_CALCULADORA_H
+
+int sumar(int n1, int n2){
+
+    return n1 + n2;
+
+}
+
+int restar(int n1, int n2){
+
+    return n1 - n2;
+
+}
+int multiplicar(int n1, int n2){
+
+    return n1 * n2;
+
+}
+
+int dividir(int n1, int n2){
+
+    return n1 / n2;
+
+}
+
+#endif //TRAVIS_CPP_CALCULADORA_H
